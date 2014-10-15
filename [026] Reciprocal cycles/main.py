9b5f61a62d4a9662f750f5470501e4bc3027234d
m.tkach <m.tkach@samsung.com>
2014-10-15 10:34:04 +0300

    Add problem 26

---
+++ [026] Reciprocal cycles/main.py
@@ -0,0 +1,22 @@
+D = 1000
+
+max_period = 0
+answer = -1
+
+for d in range(1, D):
+    z = 1
+    rem = []
+        
+    m = z % d
+    while not m in rem:
+        rem.append(m)
+        z = m * 10
+        m = z % d
+
+    p = rem[::-1].index(m) + 1 if rem[-1] > 0 else 0
+    if p > max_period:
+        max_period = p
+        answer = d
+
+print(answer)
+    
