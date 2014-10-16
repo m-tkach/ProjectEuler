3bbeaa26be3f03c0dd87f2e669c91f59b778355d
m.tkach <m.tkach@samsung.com>
2014-10-16 16:21:19 +0300

    Add problem 34

--- [034] Digit factorials/main.py
+++ [034] Digit factorials/main.py
@@ -1,9 +1,21 @@
 def calc():
-    pass
+    fact = [1, ]
+    for x in range(1, 10):
+        fact.append(fact[-1] * x)
+
+    ans = 0
+    for x in range(10, 50000):
+        s, n = 0, x
+        while n > 0:
+            d = n % 10
+            n //= 10
+            s += fact[d]
+        if s == x:
+            ans += x
+
+    return ans
 
 
 print(calc())
-    
-    
-    
+
     
