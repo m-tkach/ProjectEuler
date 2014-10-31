34a01c3e605ee40706f5061804e1eb3dbde804f4
m.tkach <m.tkach@samsung.com>
2014-10-31 20:33:16 +0200

    Add problem 56

---
+++ [056] Powerful digit sum/main.py
@@ -0,0 +1,32 @@
+A, B = 100, 100
+
+
+def pow(x, p):
+    res = 1
+    while p > 0:
+        if p & 1:
+            res *= x
+        x *= x
+        p //= 2
+    return res
+
+
+def digit_sum(x):
+    ans = 0
+    while x > 0:
+        ans += x % 10
+        x //= 10
+    return ans
+
+
+def calc():
+    ans = 0
+    for a in range(A):
+        for b in range(B):
+            temp = digit_sum(pow(a, b))
+            if temp > ans:
+                ans = temp
+    return ans
+        
+
+print(calc())
