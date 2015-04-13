4e3fcc215d916d97f41fdc765a1430efc55ba4c8
Michael Tkach <x1mike7x@gmail.com>
2015-04-13 21:09:58 +0300

    Add problem 79

--- [079] Passcode derivation/main.py
+++ [079] Passcode derivation/main.py
@@ -1,70 +1,61 @@
-"""
-1234
-
-134
-234
-123
-
-234
-134
-123
-"""
+import random
 
 
-import itertools
+def neighborhood_search(digits, space):
+    MAX = 100
 
-
-def to_str(b):
-    res = ''
-    for x in b:
-        res += str(x)
-    return res
+    for _ in range(MAX):
+        temp_digits = list(digits)
+        right = random.randint(0, len(digits) - 1)
+        left = random.randint(0, right - 1) if right > 0 else 0
+        temp_digits[left] = temp_digits[right]
+        temp_digits.pop(right)
+        ok = True
+        for item in space:
+            index = 0
+            for d in item:
+                if d not in temp_digits[index:]:
+                    ok = False
+                    break
+                else:
+                    index += temp_digits[index:].index(d) + 1
+            if not ok:
+                break
+        if ok:
+            digits = list(temp_digits)
+    return digits
 
 
 def solve(v):
-    ds = [[], [], []]
-    for i in range(3):
-        for s in v:
-            c = int(s[i])
-            if not c in ds[i]:
-                ds[i].append(c)
+    SEED = 42
+    random.seed(SEED)
 
-    best = [0] * 1000
-    for p1 in itertools.permutations(ds[0]):
-        for p2 in itertools.permutations(ds[1]):
-            for p3 in itertools.permutations(ds[2]):
-                a = []
-                for x in p1:
-                    a.append(x)
-                for x in p2:
-                    a.append(x)
-                for x in p3:
-                    a.append(x)
-                used = [False] * len(a)
-                u = 0
-                for k in v:
-                    j = 0
-                    for c in k:
-                        x = int(c)
-                        while a[j] != x:
-                            j += 1
-                        if not used[j]:
-                            used[j] = True
-                            u += 1
-                if u < len(best):
-                    best = []
-                    for i, x in enumerate(a):
-                        if used[i]:
-                            best.append(x)
+    start_stage = []
+    for item in v:
+        digits = list(item)
+        index = 0
+        for d in digits:
+            if d in start_stage[index:]:
+                index += start_stage[index:].index(d) + 1
+            else:
+                start_stage.append(d)
+                index = len(start_stage)
+
+    MAX = 25
+    ans = list(start_stage)
+    for _ in range(MAX):
+        ret = neighborhood_search(list(start_stage), v)
+        if len(ret) < len(ans):
+            ans = ret
     
-    return to_str(best)
+    return ''.join(ans)
 
 
 def calc():
     values = []
     with open('p079_keylog.txt', 'r') as ifs:
         for line in ifs:
-            values.append(line.replace('\n', ''))
+            values.append(list(line.strip()))
     return solve(values)
 
 
